e850c3a7c55512dbb15c10cc3af51850a29863d9
tjdwns335 <http://github.com/tjdwns335>
2024-03-13 10:48:32 +0900

    2번 Done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import QueryProvider from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,13 +19,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <nav>
-          <Link href="/">About</Link>
-          <Link href="/">Report</Link>
-          <Link href="/">Todos-CSR</Link>
-          <Link href="/">Todos-SSR</Link>
-        </nav>
-        {children}
+        <QueryProvider>
+          <nav>
+            <Link href="/">About</Link>
+            <Link href="/">Report</Link>
+            <Link href="/">Todos-CSR</Link>
+            <Link href="/">Todos-SSR</Link>
+          </nav>
+          {children}
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ src/app/provider.tsx
@@ -0,0 +1,11 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+
+function QueryProvider({ children }: React.PropsWithChildren) {
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
